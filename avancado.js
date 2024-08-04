76b33c92f1d8121154cd7b71ec6d6779c334b1b4
Yohran Kallahari <yohranv@gmail.com>
2024-08-04 10:16:07 -0300

    Exceções e tratamentos de erros

--- avancado.js
+++ avancado.js
@@ -32,5 +32,23 @@
 
  const frase_2 = "javaScript é incrível!"
 // Método de identificação de strings
- console.log(frase_2.startsWith("java")) // Identifica a primeira palavra da string e retorna um boolean (true, false)
- console.log(frase_2. endsWith("!"))
+ console.log(frase_2.startsWith("java")) // Identifica a primeira palavra da string e retorna um boolean (true, false), no caso desta retornará true
+ console.log(frase_2. endsWith("!")) // Identifica a ultima palavra da frase string, e retornará um boolean false.
+
+ // Exceções e tratamentos de erros
+ const idade = 15
+/*
+ if( idade < 18){
+    throw new Error("Você deve ter pelo menos 18 anos!") // criando uma nova instancia de erro e com o throw executará oque lhe foi passado, e se houver algum erro ira parar a aplicação.
+ }
+ */
+// Para o tratamento de erro try, catch irá executar o erro porem não irá parar o programa, podendo pegar o erro e joga para o usuário ou para o dev e tratar/corrigir o erro.
+ try {
+    const idade = 15
+    if( idade < 18){
+    throw new Error("Você deve ter pelo menos 18 anos!") // criando uma nova instancia de erro e com o throw executará oque lhe foi passado, e se houver algum erro ira parar a aplicação.
+ }
+ } catch (error) {
+    console.log(error.message)
+ }
+ console.log("Continuando programa!")
