74fcada0c0c529655e484989243703846fe64728
Yohran Kallahari <yohranv@gmail.com>
2024-08-04 17:42:54 -0300

    Selecionar elementos JS

---
+++ Dom.js
@@ -0,0 +1,11 @@
+// DOM - Document Object Model
+
+// Selecionar elementos
+const elementoPorID = document.getElementById("meuId")
+console.log(elementoPorID)
+
+const elementoPorID2 = document.querySelector("#meuId")
+console.log(elementoPorID2)
+
+const elementoPorID3 = document.getElementsByClassName("minhaClasse")
+console.log(elementoPorID3)
