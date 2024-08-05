351c4985a32fcea8edb04e586afbaa4b140a6c75
Yohran Kallahari <yohranv@gmail.com>
2024-08-05 09:41:40 -0300

    Trabalhando com atributos

--- Dom.js
+++ Dom.js
@@ -17,3 +17,13 @@ console.log(elementoDeTexto.textContent)
 setTimeout(()=>{
     elementoDeTexto.textContent = "Mudei de texto"
 }, 1000)
+
+// Trabalhando com atributos
+const link = document.querySelector("a") // Selecionando pela TAG, os events de dom, sempre pegarão a primeira TAG do HTML
+link.setAttribute("href", "https://horadecodar.com.br/curso") // Passar um atributo/propriedade e depois o caminho/conteúdo/link etc..
+console.log(link)
+
+console.log(link.getAttribute("href")) // verifica se há atributo passado, e mostra o conteúdo passado no atributo
+console.log(link.hasAttribute("target")) // verifica se há atributo passado e retorna um Boolean, true or false
+link.removeAttribute("target") // remove o atributo especificado
+console.log(link)
