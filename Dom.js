dc46784434e9b15f94a35f4c48cf57a08cbb011e
Yohran Kallahari <yohranv@gmail.com>
2024-08-05 09:19:14 -0300

    Manipular conteúdo de texto

--- Dom.js
+++ Dom.js
@@ -1,4 +1,4 @@
-// DOM - Document Object Model
+// DOM - Document Object Model, eventos de DOM
 
 // Selecionar elementos
 const elementoPorID = document.getElementById("meuId")
@@ -9,3 +9,11 @@ console.log(elementoPorID2)
 
 const elementoPorID3 = document.getElementsByClassName("minhaClasse")
 console.log(elementoPorID3)
+
+// Manipular conteúdo de texto
+const elementoDeTexto = document.querySelector("#meuId")
+
+console.log(elementoDeTexto.textContent)
+setTimeout(()=>{
+    elementoDeTexto.textContent = "Mudei de texto"
+}, 1000)
